7bf605f7ef5cbdfe058ed5769d82a3dbdba5aa2d
Preetham00x <preetham2006@outlook.com>
2026-07-06 13:31:09 +0530

    adding implementation for kafka producer

--- src/main/java/com/preetham/MultiDBConfig/kafka/KafkaConsumer.java
+++ src/main/java/com/preetham/MultiDBConfig/kafka/KafkaConsumer.java
@@ -1,5 +1,14 @@
 package com.preetham.MultiDBConfig.kafka;
 
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaConsumer {
 
+    @KafkaListener(topics = "test", groupId = "my-group")
+    public void consume(String message) {
+        System.out.println("Received : " + message);
+    }
 }
